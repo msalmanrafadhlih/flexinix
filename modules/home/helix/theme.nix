2706d35248f2d24656397c71a225356b93a75025
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-21 02:32:44 +0700

    Update modules/home/helix/theme.nix

--- modules/home/helix/theme.nix
+++
@@ -1,9 +0,0 @@
-{
-  ## transparent catppuccin that use borders instead of bg-color
-  # catppuccin_mocha_transparent = {
-  #   inherits = "catppuccin_mocha";
-  #   "ui.background" = { };
-  #   "ui.popup" = { };
-  #   "ui.virtual.inlay-hint" = { fg = "#6c7086"; };
-  # };
-}
