c22548a87b330979815f98c7b074d4c5e28c0370
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-23 02:51:11 +0700

    2026-03-23 02:51 | Update via SAVEFLAKE: lib/pkgs/bloodrage-plymouth.nix

--- lib/pkgs/bloodrage-plymouth.nix
+++ lib/pkgs/bloodrage-plymouth.nix
@@ -5,5 +5,5 @@ callPackage (fetchFromGitHub {
   owner = "TQ-See";
   repo = "NixPlymouth-Bloodrage";
   rev = "33d8b1262ee0f7ab20ca9e48f5fa79fae45f91e8";
-  hash = "sha256-0000000000000000000000000000000000000000000=";
+  hash = "sha256-+1OZxnxtUMkcMau4BAoOr3wJ8rGfym65cJukuusoYqA=";
 } + "/package.nix") {}
