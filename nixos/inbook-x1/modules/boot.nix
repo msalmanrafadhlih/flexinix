3484c83c568167a1e9447d62e39c82e588d2b26f
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-23 18:53:17 +0700

    2026-03-23 18:53 | Update via SAVEFLAKE: nixos/inbook-x1/modules/boot.nix

--- nixos/inbook-x1/modules/boot.nix
+++ nixos/inbook-x1/modules/boot.nix
@@ -1,46 +1,48 @@
 { pkgs, ... }:
 
 {
-  # Bootloader
-	boot.loader.timeout = 5;
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.loader.systemd-boot.configurationLimit = 5;
+  boot = {
+    loader = {
+      timeout = -1; # infinite timeout (nunggu user selamanya)
 
-	# Splash Screen
-	boot = {
-		plymouth = {
-			enable = true;
-			# THEMES = details, glow, script, spinfinity, Bloodrage,
-			# spinner, breeze, fade-in, nixos, solar, text, tribar
-			theme = "Bloodrage"; 
-			themePackages = [ pkgs.bloodrage-plymouth ];
-			extraConfig = "";
-			# logo = pkgs.fetchurl {
-			# 	url = "https://raw.githubusercontent.com/Melkor333/nixos-boot/refs/heads/main/src/evil-nixos.png";
-			# 	sha256 = "sha256-U/NgApBDrc40Q5VZkq4iuzEdOuR6Ne5QvpHbxJjg+iU=";
-			# };
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev"; # untuk UEFI
+        useOSProber = true; # detect OS lain (optional)
+        configurationLimit = 10; # limit generation
+
+        gfxmodeEfi = "auto";
+        gfxpayloadEfi = "keep";
+      };
+
+      efi.canTouchEfiVariables = true;
+    };
+
+    plymouth = {
+      enable = true;
+      theme = "Bloodrage";
+      themePackages = [ pkgs.bloodrage-plymouth ];
+    };
+
+    consoleLogLevel = 0;
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "rd.systemd.show_status=0"
+      "vt.global_cursor_default=0"
+      "rd.udev.log_level=0"
+    ];
+
+		initrd = {
+			verbose = false; # matikan output initrd
+			# systemd.enable = true;
+			kernelModules = [
+				"i915" # intel
+				# "amdgpu" # amd
+				# "nvidia-drm.modeset=1" # nvidia
+			];
 		};
-		consoleLogLevel = 0; # set level log kernel (0=sepi, 7=verbose)
-		kernelParams = [
-			# untuk cek log,
-			# hapus quit dan log_level=7 jika error,
-			"quiet" 
-			"splash"
-			"rd.systemd.show_status=0"
-			"vt.global_cursor_default=0"
-			"rd.udev.log_level=0" 
-		];
-	};
-
-
-	boot.initrd = {
-		verbose = false; # matikan output initrd
-		# systemd.enable = true;
-		kernelModules = [
-			"i915" # intel
-			# "amdgpu" # amd
-			# "nvidia-drm.modeset=1" # nvidia
-		];
-	};
+  };
 }
