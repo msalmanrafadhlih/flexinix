5d0b2f7998863a9a6bee28d66f248df6377b321e
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-29 14:54:40 +0700

    chore: remove legacy modules and configurations

--- flake.nix
+++ flake.nix
@@ -1,90 +1,80 @@
 {
-  description = "nixos config (cleaned)";
+  description = "flexinix";
+ 
+  outputs = { self, ... }@inputs: let
 
-  inputs = {
-    nixpkgs-stable.url  = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    # custom libraries
+    mylibs = import ./.lib {
+      inherit inputs self;
     };
+  in {
+    # set your profile systems here!
+    stateVersion = "25.11";
+    timezone = "Asia/Jakarta";
 
-    nix-snapd = {
-      url = "github:nix-community/nix-snapd";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    overlays                 = mylibs.overlays;       # overlays.default is the sum of all the overlays
+    packages                 = mylibs.packages;       # custom packages built against nixpkgs
+    devShells                = mylibs.devShells;
+    legacyPackages           = mylibs.legacyPackages; # applies overlays.default to nixpkgs.legacyPackages
+    systemConfigs            = mylibs.systemConfigs;
+    schemas                  = mylibs.schemas;        # not merged yet: https://github.com/NixOS/nix/pull/8892
 
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # home-manager configurations
-    racooonfig = {
-      url = "github:msalmanrafadhlih/racooonfig";
-      # url = "path:./development"; # for local testing
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixosConfigurations      = import ./nixosConfigurations      { inherit inputs; inherit (mylibs) mkConfigs; };
+    homeConfigurations       = import ./homeConfigurations       { inherit inputs; inherit (mylibs) mkConfigs; };
+    darwinConfigurations     = import ./darwinConfigurations     { inherit inputs; inherit (mylibs) mkConfigs; };
+    nixOnDroidConfigurations = import ./nixOnDroidConfigurations { inherit inputs; inherit (mylibs) mkConfigs; }; # nix-on-droid switch --flake github:msalmanrafadhlih/flexinix
   };
 
-  outputs = { self, nixpkgs, racooonfig, ... }@inputs:
-  let
-    flakeRoot = self.outPath;
-    systems   = [ "aarch64-linux" "i686-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
+  inputs = {
+    # /// nixpkgs version ///////////////////////////////////////////////
+    # /// take it from the registry /////////////////////////////////////
+    # - `nix registry add nixpkgs github:NixOS/nixpkgs/<some-branch-version>`
+    nixpkgs                  = { type = "indirect"; id = "nixpkgs"; };
+    # - nixos Stable
+    nixos-stable.url         = "github:NixOS/nixpkgs/nixos-25.11";
+    nixos-stable-lib.url     = "github:NixOS/nixpkgs/nixos-25.11?dir=lib"; # "github:nix-community/nixpkgs.lib" doesn't work
+    nixos-stable-small.url   = "github:NixOS/nixpkgs/nixos-25.11-small";
+    # - nixos Unstable
+    master.url               = "github:NixOS/nixpkgs/master";
+    nixos-unstable.url       = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-unstable-lib.url   = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
+    nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
-    mkSystem  = import ./lib/mksystem.nix {
-      inherit  nixpkgs inputs flakeRoot;
-    };
-  in
-  {
-    # custom packages Accessible through 'nix build', 'nix shell', etc
-    packages  = forAllSystems (system: import ./lib/pkgs nixpkgs.legacyPackages.${system});
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-
-    # NixOS funcion : host { arch; username; extraModules; wsl; darwin; }
-    # sudo nixos-rebuild (switch | build-vm) --flake ./#( infinix | wsl | mac ... )
-    nixosConfigurations.infinix = mkSystem "inbook-x1" {
-      system   = "x86_64-linux";
-      username = "tquilla";
-      extraModules = [ racooonfig.homeModules.default ];
-    };
-
-    nixosConfigurations.vm-aarch64 = mkSystem "vm-aarch64" {
-      system   = "aarch64-linux";
-      username = "tquilla";
-    };
-
-    nixosConfigurations.vm-aarch64-prl = mkSystem "vm-aarch64-prl" {
-      system   = "aarch64-linux";
-      username = "tquilla";
-    };
-
-    nixosConfigurations.vm-aarch64-utm = mkSystem "vm-aarch64-utm" {
-      system   = "aarch64-linux";
-      username = "tquilla";
+    # /// system-manager ////////////////////////////////////////////////
+    # - Home-Manager
+    home-manager-stable      = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixos-stable-lib"; };
+    home-manager-unstable    = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixos-unstable-lib"; };
+    # - Nix-Darwin
+    nix-darwin               = { url = "github:lnl7/nix-darwin/master"; inputs.nixpkgs.follows = "nixos-stable-lib"; };
+    darwin-stable.url        = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    darwin-unstable.url      = "github:NixOS/nixpkgs/nixpkgs-unstable"; # darwin-unstable for now (https://github.com/NixOS/nixpkgs/issues/107466)
+    # - Nix-WSL
+    wsl                      = { url = "github:nix-community/NixOS-WSL"; inputs.nixpkgs.follows = "nixos-stable-lib"; };
+    # - Nix-on-Droid
+    nix-on-droid             = { url = "github:nix-community/nix-on-droid/master";
+      inputs                 = { nixpkgs.follows = "nixos-stable-lib";
+        home-manager.follows = "home-manager-stable";
+      };
     };
 
-    nixosConfigurations.wsl = mkSystem "wsl" {
-      system   = "x86_64-linux";
-      username = "tquilla";
-      wsl      = true;
+    # /// core_ecosystems ///////////////////////////////////////////////
+    flake-schemas.url        = "github:DeterminateSystems/flake-schemas"; # tool: validator scheme for flake > `nix flake check` 
+    flake-utils.url          = "github:numtide/flake-utils"; # MultiSystem helper 
+    flake-compat             = { url = "github:edolstra/flake-compat"; flake = false; }; # tool = nonflake 
+    flake-parts              = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixos-stable-lib"; }; # tool = Modular Flake 
+    
+    # /// development_tools ////////////////////////////////////////////
+    devshell                 = { url = "github:numtide/devshell"; inputs.nixpkgs.follows = "nixos-stable-lib"; };
+    devenv                   = { url = "github:cachix/devenv";
+      inputs                 = { flake-compat.follows = "flake-compat";
+        git-hooks.follows    = "git-hooks";
+      };
     };
 
-    darwinConfigurations.macbook = mkSystem "darwin" {
-      system   = "aarch64-darwin";
-      username = "tquilla";
-      darwin   = true;
-    };
+    # /// security/secret_tools ////////////////////////////////////////
+    sops-nix.url             = "github:Mic92/sops-nix";
+
+    # /// package_managers /////////////////////////////////////////////
+    nur.url                  = "github:nix-community/NUR";
   };
 }
