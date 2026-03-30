6741ae331c748c2e1f33e38fffbe32fc32ea8519
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-30 22:12:47 +0700

    feat(home): add shared shell configurations
    Initial additions of shared configurations for bash, fish, and zsh.

---
+++ home/shared/shell/fish.nix
@@ -0,0 +1,23 @@
+{ pkgs, inputs, lib, ... }:
+
+{
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = lib.strings.concatStrings (lib.strings.intersperse "\n" [
+      "source ${inputs.theme-bobthefish}/functions/fish_prompt.fish"
+      "source ${inputs.theme-bobthefish}/functions/fish_right_prompt.fish"
+      "source ${inputs.theme-bobthefish}/functions/fish_title.fish"
+      (builtins.readFile ./config.fish)
+      "set -g SHELL ${pkgs.fish}/bin/fish"
+    ]);
+
+    plugins = map (n: {
+      name = n;
+      src  = inputs.${n};
+    }) [
+      "fish-fzf"
+      "fish-foreign-env"
+      "theme-bobthefish"
+    ];
+  };
+}
