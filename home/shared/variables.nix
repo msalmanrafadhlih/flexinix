b478a338b5700890de05e9a748115b899b289465
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-30 22:12:32 +0700

    feat(home): add shared home-manager base configurations
    Initial additions of shared home-manager entry and variable configurations.

---
+++ home/shared/variables.nix
@@ -0,0 +1,30 @@
+{ pkgs, isDarwin, ... }:
+let
+  # For our MANPAGER env var
+  # https://github.com/sharkdp/bat/issues/1145
+  manpager = (pkgs.writeShellScriptBin "manpager" (if isDarwin then ''
+    sh -c 'col -bx | bat -l man -p'
+    '' else ''
+    cat "$1" | col -bx | bat --language man --style plain
+  ''));
+in
+
+{
+  #---------------------------------------------------------------------
+  # Env vars and dotfiles
+  #---------------------------------------------------------------------
+  home.sessionVariables = {
+    LANG = "en_US.UTF-8";
+    LC_CTYPE = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    EDITOR = "hx";
+    PAGER = "less -FirSwX";
+    MANPAGER = "${manpager}/bin/manpager";
+
+    AMP_API_KEY = "op://Private/Amp_API/credential";
+    OPENAI_API_KEY = "op://Private/OpenAPI_Personal/credential";
+  } // (if isDarwin then {
+    # See: https://github.com/NixOS/nixpkgs/issues/390751
+    DISPLAY = "nixpkgs-390751";
+  } else {});
+}
