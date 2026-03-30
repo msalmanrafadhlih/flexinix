6111441487107c28f7f86fb69004bc3c55ff8e21
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-30 21:58:47 +0700

    feat(.lib): add custom packages and system configuration modules
    Added local package definitions for bloodrage-plymouth and desktopify-lite, and restructured system configuration management.

---
+++ .lib/packages/default.nix
@@ -0,0 +1,18 @@
+inputs: let
+  lib = inputs.nixos-stable.lib;
+  forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+in forAllSystems (
+  system: let
+    pkgs = import inputs.nixpkgs {
+      inherit system;
+      config = (import ../nixpkgs inputs).config;
+      overlays = [ (import ./overlays.nix inputs).default ];
+    };
+  in
+
+  # custom Packages
+  {
+    desktopify-lite = pkgs.callPackage ./desktopify-lite.nix {};
+    bloodrage-plymouth = pkgs.callPackage ./bloodrage-plymouth.nix {};
+  }
+)
---
+++ .lib/systemConfigs/systemModule.nix
@@ -0,0 +1,15 @@
+{ pkgs, inputs, ...}: {
+  nixpkgs.hostPlatform = "x86_64-linux";
+  system-manager.allowAnyDistro = true;
+
+  environment = {
+    etc = {
+      "foo.conf".text = ''
+        launch_the_rockets = true
+      '';
+    };
+    systemPackages = [
+      inputs.system-manager.packages.${pkgs.stdenv.hostPlatform.system}.system-manager
+    ];
+  };
+}
