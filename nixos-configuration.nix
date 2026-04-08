ca6335ac4afc3238a8be446130ead1ce5ef8aba3
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-08 18:42:56 +0700

    test(modules): fix args modules recursion

--- nixos-configuration.nix
+++ nixos-configuration.nix
@@ -3,7 +3,7 @@
   # NixOS funcion : hostname { arch; username; extraModules; wsl; darwin; }
   # sudo nixos-rebuild (switch | build-vm) --flake ./#( infinix | wsl | mac ... )
 
-  infinix = mkConfigs.nixos "stable" {
+  infinix = mkConfigs.nixos "unstable" {
     hostname     = "inbook-x1";
     username     = "tquilla";
     system       = "x86_64-linux";
