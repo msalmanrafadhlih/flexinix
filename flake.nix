d4399d73bab568192591d2a11d78f78c33dcd0f2
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-08 21:22:26 +0700

    test(modules): fix args modules recursion

--- flake.nix
+++ flake.nix
@@ -100,4 +100,17 @@
       inputs.nixpkgs.follows = "nixos-unstable";
     };
   };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
+      "https://devenv.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
 }
