9995b55b6d7156104567b370ccffad1a23800c62
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-10 10:14:21 +0700

    fix(plymouth): add delay for plymouth

--- hosts/Nix-inbook-x1/modules/boot.nix
+++ hosts/Nix-inbook-x1/modules/boot.nix
@@ -37,10 +37,10 @@ in {
   kernelParams = [
     "quiet"
     "splash"
-    # "boot.shell_on_fail"
-    # "loglevel=3"
+    "boot.shell_on_fail"
+    "loglevel=3"
     "rd.systemd.show_status=0"
-    # "vt.global_cursor_default=0"
+    "vt.global_cursor_default=0"
     "rd.udev.log_level=0"
     "plymouth.delay=5"
   ];
