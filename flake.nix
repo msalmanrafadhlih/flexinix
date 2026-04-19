d1ca556884b5eb2201436b6aea593763eb336709
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-19 09:32:12 +0700

    2026-04-19 09:32 | Update via SAVEFLAKE: flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     let
       # set your default variables here!
       var = {
-        editor = "hx"; # hx / nvim,
+        editor = "hx"; # hx / nvim / vi,
         locale = "en_US.UTF-8";
         timezone = "Asia/Jakarta";
 
@@ -20,7 +20,7 @@
       mylibs = import ./.lib { inherit inputs self; };
     in
     {
-      inherit var;
+      inherit (var) editor local timezone stateVersion;
       legacyPackages = mylibs.legacyPackages; # applies overlays.default to nixpkgs.legacyPackages
       devShells = mylibs.devShells;
       overlays = mylibs.overlays; # overlays.default is the sum of all the overlays
