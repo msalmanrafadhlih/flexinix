34ea289469aa70f485019ea4084b6d2473b49c85
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-19 09:27:36 +0700

    2026-04-19 09:27 | Update via SAVEFLAKE: homeModules/default.nix

--- homeModules/default.nix
+++ homeModules/default.nix
@@ -1,4 +1,11 @@
 { flakeRoot, ... }:
+let
+  editor = {
+    nvim = ./neovim;
+    hx   = ./helix;
+    vim  = ./vim;
+  };
+in
 {
   imports = [
     ./variables.nix
@@ -8,15 +15,15 @@
     ./serpl.nix # tui find and replace tool, good for integratinng with terminal editors
     ./btop.nix
     ./git.nix
-    ./helix
     ./shell
     ./yazi
     ./tmux
 
     # ./zellij
     # ./wezterm
+    editor.${flakeRoot.editor}
   ];
-  
+
   # Disabled for now since we mismatch our versions. See flake.nix for details.
   home.enableNixpkgsReleaseCheck = false;
   home.stateVersion = flakeRoot.stateVersion.linux;
