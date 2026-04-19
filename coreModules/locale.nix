1ce0a4053eda01727831e02134bb344a54b0081b
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-19 09:27:36 +0700

    2026-04-19 09:27 | Update via SAVEFLAKE: coreModules/locale.nix

--- coreModules/locale.nix
+++ coreModules/locale.nix
@@ -5,14 +5,15 @@
   i18n.defaultLocale = flakeRoot.locale;
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = flakeRoot.locale;
     LC_IDENTIFICATION = flakeRoot.locale;
     LC_MEASUREMENT = flakeRoot.locale;
-    LC_MONETARY = flakeRoot.locale;
-    LC_NAME = flakeRoot.locale;
-    LC_NUMERIC = flakeRoot.locale;
-    LC_PAPER = flakeRoot.locale;
     LC_TELEPHONE = flakeRoot.locale;
+    LC_MONETARY = flakeRoot.locale;
+    LC_ADDRESS = flakeRoot.locale;
+    LC_NUMERIC = flakeRoot.locale;
+    LC_CTYPE = flakeRoot.locale;
+    LC_PAPER = flakeRoot.locale;
+    LC_NAME = flakeRoot.locale;
     LC_TIME = flakeRoot.locale;
   };
 }
