dad2f4643770eee77c72d346316c718297790fcd
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-21 21:16:51 +0700

    2026-04-21 21:16 | Update via SAVEFLAKE: .lib/packages/cursor-theme.nix

--- .lib/packages/cursor-theme.nix
+++ .lib/packages/cursor-theme.nix
@@ -1,7 +1,5 @@
 { stdenv, fetchFromGitHub }:
-let
-  selectedTheme = "Kafka";
-in
+
 stdenv.mkDerivation {
   pname = "cursor-memes";
   version = "1.0";
@@ -9,18 +7,32 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "TQ-See";
     repo = "Cursors-memes";
-    rev = "14b8b34646244d34d56f43757e56fb190d396b2b";
+    rev = "3ec9c73870a036c7b3f8d8f3741554f9e70e9030";
 
     # WAJIB: Tambahkan ini agar Nix hanya mengambil folder tema tersebut
-    sparseCheckout = [ selectedTheme ];
+    sparseCheckout = [
+      "Kafka"                           # red
+      "Crelly-Cursor-Pack"              # green
+      "Ju-Fufu"                         # yellow
+      "Kiana-(Herrscher-of-Flamescion)" # grey
+      "Ellen-Joe"                       # black
+      "Erika-Furudo"                    # blue
+    ];
 
-    sha256 = "sha256-LEDpYOvbtNPscuwoxv713O43WjRBuVrstutrsVvnnIU=";
+    sha256 = "";
   };
 
   installPhase = ''
     # Buat direktori tujuan standar kursor Linux
     mkdir -p $out/share/icons
-    cp -r ${selectedTheme} $out/share/icons/
+
+    for d in *; do
+      # Cek apakah itu direktori dan memiliki file index.theme di dalamnya
+      if [ -d "$d" ] && [ -f "$d/index.theme" ]; then
+        echo "Installing cursor pack: $d"
+        cp -pr "$d" $out/share/icons/
+      fi
+    done
 
     # Opsional: Hapus folder 'default' jika tidak sengaja terikut 
     # agar tidak konflik dengan Stylix
