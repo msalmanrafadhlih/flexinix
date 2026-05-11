2ba9eff99cb9cfe84808390b86df0907a831ab22
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-11 20:42:28 +0700

    2026-05-11 20:41 | Update via SAVEFLAKE: .lib/packages/qylock-sddm-theme.nix

--- .lib/packages/qylock-sddm-theme.nix
+++ .lib/packages/qylock-sddm-theme.nix
@@ -15,19 +15,37 @@ stdenv.mkDerivation {
     sparseCheckout = [ themeSubPath ];
     sha256 = "sha256-bX1jo+4xgh5Jgtjqk82G2b3jMkeMvIiAIk4ICWOtLq0=";
   };
-
   # SDDM theme biasanya butuh struktur folder yang spesifik
   installPhase = ''
     runHook preInstall
+    mkdir -p $out/share/sddm/themes
 
-    mkdir -p $out/share/sddm/themes/orbital
-    mkdir -p $out/share/sddm/themes/tape
-    cp -r ${themeSubPath}/orbital/* $out/share/sddm/themes/orbital/
-    cp -r ${themeSubPath}/tape/* $out/share/sddm/themes/tape/
+    link_theme() {
+      local dir="$1"
+
+      if [ -f "$dir/metadata.desktop" ] \
+        && [ -f "$dir/theme.conf" ]; then
+
+        local name
+        name="$(basename "$dir")"
+
+        echo "Linking SDDM theme: $name"
+
+        ln -s "$PWD/$dir" \
+          "$out/share/sddm/themes/$name"
+      fi
+    }
+
+    # cek jika themeSubPath sendiri adalah theme
+    link_theme "${themeSubPath}"
+
+    # cek subfolder
+    for dir in ${themeSubPath}/*; do
+      [ -d "$dir" ] || continue
+      link_theme "$dir"
+    done
 
     runHook postInstall
   '';
 }
 
-    # mkdir -p $out/share/sddm/themes/${selectedTheme}
-    # cp -r ${themeSubPath}/* $out/share/sddm/themes/${selectedTheme}/
