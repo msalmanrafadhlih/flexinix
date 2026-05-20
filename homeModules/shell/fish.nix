ed6670e06b1f9a4982763fe2382b7e1bcda898e1
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-20 20:35:08 +0700

    2026-05-20 20:34 | Update via SAVEFLAKE: homeModules/shell/fish.nix

--- homeModules/shell/fish.nix
+++ homeModules/shell/fish.nix
@@ -1,23 +1,36 @@
-{ pkgs, inputs, lib, ... }:
+{
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
+let
+  inp = inputs.racooonfig.inputs;
+in
 
 {
   programs.fish = {
     enable = true;
-    interactiveShellInit = lib.strings.concatStrings (lib.strings.intersperse "\n" [
-      "source ${inputs.theme-bobthefish}/functions/fish_prompt.fish"
-      "source ${inputs.theme-bobthefish}/functions/fish_right_prompt.fish"
-      "source ${inputs.theme-bobthefish}/functions/fish_title.fish"
-      (builtins.readFile ./config.fish)
-      "set -g SHELL ${pkgs.fish}/bin/fish"
-    ]);
+    interactiveShellInit = lib.strings.concatStrings (
+      lib.strings.intersperse "\n" [
+        "source ${inp.theme-bobthefish}/functions/fish_prompt.fish"
+        "source ${inp.theme-bobthefish}/functions/fish_right_prompt.fish"
+        "source ${inp.theme-bobthefish}/functions/fish_title.fish"
+        (builtins.readFile ./config.fish)
+        "set -g SHELL ${pkgs.fish}/bin/fish"
+      ]
+    );
 
-    plugins = map (n: {
-      name = n;
-      src  = inputs.${n};
-    }) [
-      "fish-fzf"
-      "fish-foreign-env"
-      "theme-bobthefish"
-    ];
+    plugins =
+      map
+        (n: {
+          name = n;
+          src = inp.${n};
+        })
+        [
+          "fish-fzf"
+          "fish-foreign-env"
+          "theme-bobthefish"
+        ];
   };
 }
