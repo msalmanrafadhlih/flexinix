6a23c1a9cfa0c54547eeb4179353100883c8f942
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-21 19:58:34 +0700

    2026-05-21 19:58 | Update via SAVEFLAKE: flake.nix

--- flake.nix
+++ flake.nix
@@ -21,8 +21,9 @@
     in
     {
       inherit (var) editor locale timezone stateVersion;
-      inherit (mylibs) nixpkgs packages;
+      inherit (mylibs) nixpkgs;
       
+      packages       = mylibs.packages;
       legacyPackages = mylibs.legacyPackages; # applies overlays.default to nixpkgs.legacyPackages
       devShells      = mylibs.devShells;
       overlays       = mylibs.overlays; # overlays.default is the sum of all the overlays
