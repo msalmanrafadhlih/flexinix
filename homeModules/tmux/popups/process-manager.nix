3585ee84208a3b8fc058c3a0087a36b0a257630e
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-21 19:41:30 +0700

    2026-05-21 19:41 | Update via SAVEFLAKE: homeModules/tmux/popups/process-manager.nix

--- homeModules/tmux/popups/process-manager.nix
+++ homeModules/tmux/popups/process-manager.nix
@@ -1,6 +1,6 @@
-{ flakeRoot, system, ... }:
+{ inputs, system, ... }:
 let
-  rip = "${flakeRoot.packages.${system}.rip}/bin/rip";
+  rip = "${inputs.racooonfig.packages.${system}.rip}/bin/rip";
 in
 {
   programs.tmux.extraConfig = ''
