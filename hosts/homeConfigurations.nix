6d4a461b41e032f8908aeba4f2c64d2fd0c104a9
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-21 16:05:39 +0700

    2026-05-21 16:05 | Update via SAVEFLAKE: hosts/homeConfigurations.nix

--- hosts/homeConfigurations.nix
+++ hosts/homeConfigurations.nix
@@ -1,16 +1,16 @@
-{ inputs, mkConfigs, ... }: {
+{ inputs, home, ... }: {
 
   # HomeManager function : stability { hostname; username; system; extraHomeModules; (boolean: nixOnDroid; wsl; darwin;)}
   # home-manager rebuild switch --flake /etc/nixos#hostname@username
 
-  "infinix@tquilla" = mkConfigs.home "unstable" {
+  "infinix@tquilla" = home "unstable" {
     hostname         = "inbook-x1";
     username         = "tquilla";
     system           = "x86_64-linux";
     extraHomeModules = [ inputs.racooonfig.default ];
   };
 
-  "wsl@tquilla" = mkConfigs.home "stable" {
+  "wsl@tquilla" = home "stable" {
     hostname         = "wsl";
     username         = "tquilla";
     system           = "x86_64-linux";
