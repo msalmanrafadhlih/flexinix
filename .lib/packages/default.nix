317be6c9d0d450613a2433093abaec919d673e46
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-21 16:05:39 +0700

    2026-05-21 16:05 | Update via SAVEFLAKE: .lib/packages/default.nix

--- .lib/packages/default.nix
+++ .lib/packages/default.nix
@@ -5,10 +5,20 @@ let
   # Definisikan paket individual agar bisa dipanggil secara mandiri
   desktopify-lite = pkgs.callPackage ./desktopify-lite.nix {};
   rip             = pkgs.callPackage ./process-manager.nix {};
+
+  # overlay-packages
+  bloodrage-plymouth = pkgs.callPackage ./bloodrage-plymouth.nix {};
+  qylock-sddm-theme  = pkgs.callPackage ./qylock-sddm-theme.nix {};
+  my-grub-theme      = pkgs.callPackage ./my-grub-theme.nix {};
+  cursor-memes       = pkgs.callPackage ./cursor-theme.nix {};
 in
 {
   # Ekspos paket individual (for spesifik packages ex. 'nix build .#desktopify-lite')
   inherit
+    bloodrage-plymouth
+    qylock-sddm-theme
+    my-grub-theme
+    cursor-memes
     desktopify-lite
     rip
     ;
