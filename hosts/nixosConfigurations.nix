d2506a2b358f7c775fab11008fb001ff9332bfed
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-21 16:05:39 +0700

    2026-05-21 16:05 | Update via SAVEFLAKE: hosts/nixosConfigurations.nix

--- hosts/nixosConfigurations.nix
+++ hosts/nixosConfigurations.nix
@@ -1,16 +1,16 @@
-{ mkConfigs, ... }: {
+{ nixos, ... }: {
 
   # NixOS funcion : hostname { arch; username; extraModules; wsl; darwin; }
   # sudo nixos-rebuild (switch | build-vm) --flake ./#( infinix | wsl | mac ... )
 
-  infinix = mkConfigs.nixos "unstable" {
+  infinix = nixos "unstable" {
     hostname     = "inbook-x1";
     username     = "tquilla";
     system       = "x86_64-linux";
     extraModules = [ ./Nix-inbook-x1/home.nix ];
   };
 
-  wsl = mkConfigs.nixos "stable" {
+  wsl = nixos "stable" {
     hostname = "wsl";
     username = "tquilla";
     system   = "x86_64-linux";
