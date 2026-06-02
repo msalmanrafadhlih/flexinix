86d6a98eb96174dfac821d2c990f426d583aca2e
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-02 12:54:59 +0700

    Update via SAVEFLAKE - 2026-06-02 12:54
    Update file: hosts/Nix-inbook-x1/configuration.nix
    
    Changes:
    -{ ... }:
    +{ mapFile, ... }:
    -    ./modules/audio.nix
    -    ./modules/autoupdate.nix
    -    ./modules/boot.nix
    -    ./modules/bluetooth.nix
    -    ./modules/network.nix
    -    ./modules/hardware.nix
    -    ./modules/services.nix
    -    # ./modules/power.nix
    -    ./modules/touchpad.nix
    -    ./modules/fileSystems.nix
    -
    -  ];

--- hosts/Nix-inbook-x1/configuration.nix
+++ hosts/Nix-inbook-x1/configuration.nix
@@ -1,17 +1,7 @@
-{ ... }:
+{ mapFile, ... }:
 {
   imports = [
-    ./modules/audio.nix
-    ./modules/autoupdate.nix
-    ./modules/boot.nix
-    ./modules/bluetooth.nix
-    ./modules/network.nix
-    ./modules/hardware.nix
-    ./modules/services.nix
-    # ./modules/power.nix
-    ./modules/touchpad.nix
-    ./modules/fileSystems.nix
-    
     ./hardware-configuration.nix
-  ];
+  ]
+  ++ mapFile ./modules [ ] { };
 }
