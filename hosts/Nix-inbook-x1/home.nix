ec196272e9b5c03bdac0118f4b3acf8d38cedd84
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-02 15:47:19 +0700

    Update via SAVEFLAKE - 2026-06-02 15:47
    Update file: hosts/Nix-inbook-x1/home.nix
    
    Changes:
    -    # fileManager = "thunar";

--- hosts/Nix-inbook-x1/home.nix
+++ hosts/Nix-inbook-x1/home.nix
@@ -19,7 +19,7 @@ in
 
   racooonfig = {
     enable = true;
-    # fileManager = "thunar";
+    fileManager = "dolphin";
     displayManager = "sddm";
     desktopManager = [ "plasma" ];
     gamemode = {
