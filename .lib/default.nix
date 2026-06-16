125cfc596a8da408915aed7ce3faee039091001e
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-16 17:02:28 +0700

    Update via SAVEFLAKE - 2026-06-16 17:01
    Update file: .lib/default.nix
    
    Changes:
    -  devShells = import ./devShells args;
    -  # systemConfig   = import ./systemConfigs      args;
    -    devShells

--- .lib/default.nix
+++ .lib/default.nix
@@ -5,17 +5,14 @@ let
   packages = inputs.racooonfig.packages;
   legacyPackages = inputs.racooonfig.legacyPackages;
 
-  devShells = import ./devShells args;
   mkConfigs = import ./mkConfigs.nix args;
   overlays = import ./overlays args;
-  # systemConfig   = import ./systemConfigs      args;
 in
 {
   inherit
     nixpkgs
     mkConfigs
     overlays
-    devShells
     schemas
     legacyPackages
     packages
