9ebec8f933d627c0b2d56a02862af44c940ac01d
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-22 22:57:35 +0700

    Update via SAVEFLAKE - 2026-06-22 22:57
    Update file: hosts/Nix-inbook-x1/modules/touchpad.nix
    
    Changes:
    - services = {
    -	libinput = {
    -	    enable = true;
    -	    touchpad = {
    -	        tapping = true;
    -	        naturalScrolling = true;
    -	        disableWhileTyping = true;
    -	        sendEventsMode = "disabled-on-external-mouse";
    -	    };
    -	};
    -  #   udev = {
    -  #       enable = true;
    -  #       extraRules = let
    -		#     checkMice = pkgs.writeScript "check-mice" ''
    -		#     #!${pkgs.bash}/bin/bash
    -		#     # Check mice
    -		#     internal=(
    -		#       'Raydium Corporation Raydium Touch System'
    -		#       'Synaptics TM3289-021'
    -		#       'TPPS/2 Elan TrackPoint'
    -		#     )
    -		#     inhibitTrackpad=0
    -		#     for nameFile in /sys/class/input/mouse*/device/name; do
    -		#       name=$(cat "$nameFile")
    -		#       found=
    -		#       for (( i=0; i < ''${#internal[@]}; i += 1 )); do
    -		#         if [[ ''${internal[$i]} == $name ]]; then
    -		#           found=yes
    -		#           break
    -		#         fi
    -		#       done
    -		#       if [[ ! $found ]]; then
    -		#         # There is at least one external mouse connected
    -		#         inhibitTrackpad=1
    -		#         break
    -		#       fi
    -		#     done
    -		#     echo "$inhibitTrackpad" > /sys/class/input/mouse1/device/inhibited
    -		#   '';
    -		# in ''
    -		#   # Detect addition or removal of mouse devices, then enable or disable trackpad when external mouse is attached
    -		#   SUBSYSTEM=="input", DEVPATH=="*/input/*/mouse*", RUN+="${checkMice}"
    -		# '';
    -  #   };
    +
    -

--- hosts/Nix-inbook-x1/modules/touchpad.nix
+++ hosts/Nix-inbook-x1/modules/touchpad.nix
@@ -1,55 +1,60 @@
 { pkgs, ... }:
 
 {
- services = {
-	libinput = {
-	    enable = true;
-	    touchpad = {
-	        tapping = true;
-	        naturalScrolling = true;
-	        disableWhileTyping = true;
-	        sendEventsMode = "disabled-on-external-mouse";
-	    };
-	};
+  services = {
+    libinput = {
+      enable = true;
+      touchpad = {
+        tapping = true;
+        naturalScrolling = true;
+        disableWhileTyping = true;
+        sendEventsMode = "disabled-on-external-mouse";
+      };
+    };
 
-  #   udev = {
-  #       enable = true;
-  #       extraRules = let
-		#     checkMice = pkgs.writeScript "check-mice" ''
-		#     #!${pkgs.bash}/bin/bash
+    udev.extraRules = ''
+      # Disable autosuspend for ROBOT KM3500 USB Receiver
+      # `Bus 003 Device 004: ID 32c2:0066  2.4G Wireless`
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="32c2", ATTRS{idProduct}=="0066", ATTR{power/autosuspend}="-1"
+    '';
 
-		#     # Check mice
-		#     internal=(
-		#       'Raydium Corporation Raydium Touch System'
-		#       'Synaptics TM3289-021'
-		#       'TPPS/2 Elan TrackPoint'
-		#     )
+    #   udev = {
+    #       enable = true;
+    #       extraRules = let
+    #     checkMice = pkgs.writeScript "check-mice" ''
+    #     #!${pkgs.bash}/bin/bash
 
-		#     inhibitTrackpad=0
+    #     # Check mice
+    #     internal=(
+    #       'Raydium Corporation Raydium Touch System'
+    #       'Synaptics TM3289-021'
+    #       'TPPS/2 Elan TrackPoint'
+    #     )
 
-		#     for nameFile in /sys/class/input/mouse*/device/name; do
-		#       name=$(cat "$nameFile")
-		#       found=
-		#       for (( i=0; i < ''${#internal[@]}; i += 1 )); do
-		#         if [[ ''${internal[$i]} == $name ]]; then
-		#           found=yes
-		#           break
-		#         fi
-		#       done
-		#       if [[ ! $found ]]; then
-		#         # There is at least one external mouse connected
-		#         inhibitTrackpad=1
-		#         break
-		#       fi
-		#     done
+    #     inhibitTrackpad=0
 
-		#     echo "$inhibitTrackpad" > /sys/class/input/mouse1/device/inhibited
-		#   '';
-		# in ''
-		#   # Detect addition or removal of mouse devices, then enable or disable trackpad when external mouse is attached
-		#   SUBSYSTEM=="input", DEVPATH=="*/input/*/mouse*", RUN+="${checkMice}"
-		# '';
-  #   };
+    #     for nameFile in /sys/class/input/mouse*/device/name; do
+    #       name=$(cat "$nameFile")
+    #       found=
+    #       for (( i=0; i < ''${#internal[@]}; i += 1 )); do
+    #         if [[ ''${internal[$i]} == $name ]]; then
+    #           found=yes
+    #           break
+    #         fi
+    #       done
+    #       if [[ ! $found ]]; then
+    #         # There is at least one external mouse connected
+    #         inhibitTrackpad=1
+    #         break
+    #       fi
+    #     done
+
+    #     echo "$inhibitTrackpad" > /sys/class/input/mouse1/device/inhibited
+    #   '';
+    # in ''
+    #   # Detect addition or removal of mouse devices, then enable or disable trackpad when external mouse is attached
+    #   SUBSYSTEM=="input", DEVPATH=="*/input/*/mouse*", RUN+="${checkMice}"
+    # '';
+    #   };
   };
 }
-
