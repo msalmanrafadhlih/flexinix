944df461c453f10e1cf6b4463b96ebf681146663
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-27 20:54:39 +0700

    Update via SAVEFLAKE - 2026-06-27 20:54
    Update file: hosts/Nix-inbook-x1/modules/secrets.nix
    
    Changes:
    -{inputs, ...}: {
    +{ inputs, pkgs, ... }: {
    +
    +

--- hosts/Nix-inbook-x1/modules/secrets.nix
+++ hosts/Nix-inbook-x1/modules/secrets.nix
@@ -1,3 +1,28 @@
-{inputs, ...}: {
+{ inputs, pkgs, ... }: {
+  environment = {
+    systemPackages = with pkgs; [
+      age
+      sops
+    ];
+  };
+
   imports = [ inputs.sops-nix.nixosModules.sops ];
+  sops = {
+    defaultSopsFormat = "yaml"; # json, binary, ini
+    defaultSopsFile = ../../../secrets.yaml;
+    age = {
+      keyFile = "/srv/share/files/secrets/age/key.txt";
+      generateKey = false; # auto generate key.txt if file corrupt or missing
+      validateSopsFiles = true;
+    };
+
+    # secrets.tquilla_password = {
+    #   # path = "/srv/share/files/secret";
+    #   owner = "tquilla";
+    #   neededForUsers = true;
+    #   restartUnits = [
+    #     "nginx.service"
+    #   ];
+    # };
+  };
 }
