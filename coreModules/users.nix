8148ecf2c3509900574580cb3a869dd5b0be2625
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-01 23:47:05 +0700

    Update via SAVEFLAKE - 2026-07-01 23:47
    Update file: coreModules/users.nix
    
    Changes:
    -{ pkgs, config, ... }:
    -let
    -in
    -{
    +{ pkgs, config, ... }: {
    -  users.groups.service = {};
    -      hashedPasswordFile = config.sops.secrets.tquilla_password.path;
    -        "users" "wheel"   # sudo
    -        "networkmanager"  # Connection(Wifi) Manager
    -        "docker"          # Container Dev
    -        "libvirtd" "kvm"  # VM testing
    -        "adbusers"        # Android Dev
    -        "video" "audio"   # Dev tools kadang butuh
    -        "i2c"             # hardware hacking
    -        "input"           # keylogging keyboard, mouse
    -      # `ssh-keygen -t ed25519 && cat ~/.ssh/id_ed25519.pub`
    -        config.sops.secrets."github/authorized_keys".path
    -    	# openssh.authorizedKeys.keyFiles = [
    -      #   # open `https://github.com/gituhb-username.keys`
    -    	#   (builtins.fetchurl {
    -    	#     url = "https://github.com/github-username.keys";
    -    	#     sha256 = "sha256:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
    -    	#   })
    -    	# ];

--- coreModules/users.nix
+++ coreModules/users.nix
@@ -1,12 +1,9 @@
 # ./modules/users.nix
-{ pkgs, config, ... }:
-let
-in
-{
+{ pkgs, config, ... }: {
   # services.getty.autologinUser = "gamemode";
   users.mutableUsers = false;
 
-  users.groups.service = {};
+  users.groups.service = { };
 
   users.users = {
     tquilla = {
@@ -15,29 +12,30 @@ in
       shell = pkgs.zsh;
       # to get password hash = `mkpasswd -m sha-512` // sha-256
       # or use `openssl passwd -6`
-      hashedPasswordFile = config.sops.secrets.tquilla_password.path; 
+      hashedPasswordFile = config.sops.secrets.tquilla_password.path;
       extraGroups = [
-        "users" "wheel"   # sudo
-        "networkmanager"  # Connection(Wifi) Manager
-        "docker"          # Container Dev
-        "libvirtd" "kvm"  # VM testing
-        "adbusers"        # Android Dev
-        "video" "audio"   # Dev tools kadang butuh
-        "i2c"             # hardware hacking
-        "input"           # keylogging keyboard, mouse
+        "users"
+        "wheel" # sudo
+        "networkmanager" # Connection(Wifi) Manager
+        "docker" # Container Dev
+        "libvirtd"
+        "kvm" # VM testing
+        "adbusers" # Android Dev
+        "video"
+        "audio" # Dev tools kadang butuh
+        "i2c" # hardware hacking
+        "input" # keylogging keyboard, mouse
       ];
 
-      # `ssh-keygen -t ed25519 && cat ~/.ssh/id_ed25519.pub` 
+      # `ssh-keygen -t ed25519 && cat ~/.ssh/id_ed25519.pub`
       openssh.authorizedKeys.keyFiles = [
-        config.sops.secrets."github/authorized_keys".path
+        # config.sops.secrets."github/authorized_keys".path # Not worked
+        # open `https://github.com/gituhb-username.keys`
+        (builtins.fetchurl {
+          url = "https://github.com/msalmanrafadhlih.keys";
+          sha256 = "sha256-ZaJ76VzOkvSEQA8Tf2dco+3UapOw4yK4nWK1SV52/BM=";
+        })
       ];
-    	# openssh.authorizedKeys.keyFiles = [
-      #   # open `https://github.com/gituhb-username.keys`
-    	#   (builtins.fetchurl {
-    	#     url = "https://github.com/github-username.keys";
-    	#     sha256 = "sha256:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    	#   })
-    	# ];
     };
   };
 
